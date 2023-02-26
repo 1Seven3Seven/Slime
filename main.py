f08d1550f3ebfd2fc1ff5cda1526042b5377c3cb
Peter Burgess <pburgess2003@gmail.com>
2023-02-26 19:01:09 +1030

    First iteration of the point class. And a simple demo.

---
+++ main.py
@@ -0,0 +1,74 @@
+import math
+
+import pygame
+
+
+class Point:
+    def __init__(self, position: list[float, float], direction: float, playground_size: tuple[int, int]):
+        """
+        :param position: The starting position of the point.
+        :param direction: The direction, in radians, to initially move in.
+        :param playground_size: The size of the area in which the point moves.
+        """
+
+        self.position: list[float, float] = position
+        self.direction: float = direction
+
+        self.vector = [math.cos(direction), -math.sin(direction)]
+
+        self.playground_size: tuple[int, int] = playground_size
+
+    def move(self):
+        """
+Moves the point in the direction of its vector.
+Bounces off the edges of the playground.
+        """
+
+        self.position[0] += self.vector[0]
+        if self.position[0] < 0:
+            self.position[0] = 0
+            self.vector[0] = -self.vector[0]
+        elif self.position[0] > self.playground_size[0]:
+            self.position[0] = self.playground_size[0]
+            self.vector[0] = -self.vector[0]
+
+        self.position[1] += self.vector[1]
+        if self.position[1] < 0:
+            self.position[1] = 0
+            self.vector[1] = -self.vector[1]
+        elif self.position[1] > self.playground_size[1]:
+            self.position[1] = self.playground_size[1]
+            self.vector[1] = -self.vector[1]
+
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("Slime Time")
+    window_size = (500, 500)
+    screen = pygame.display.set_mode(window_size)
+    clock = pygame.time.Clock()
+
+    points: list[Point] = []
+    for r in range(1000):
+        points.append(
+            Point([250, 250], 2 * math.pi * r / 1000, window_size)
+        )
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit(0)
+
+        screen.fill((0, 0, 0))
+
+        for point in points:
+            pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(point.position[0], point.position[1], 1, 1))
+
+            point.move()
+
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
